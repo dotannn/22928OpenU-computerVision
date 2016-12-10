20dd0b0140eefd672a739fe7a2c65faac9e7f2f2
Dotan <dotan1988@gmail.com>
2016-12-10 23:26:18 +0200

    add all files

--- maman12/part1.py
+++ maman12/part1.py
@@ -8,10 +8,8 @@ Created on Tue Nov 29 15:41:55 2016
 
 ### imports
 import time
-import cPickle, gzip, numpy
-import cv2
+import cPickle, gzip
 import numpy as np
-from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
@@ -33,33 +31,17 @@ class Timer(object):
         
 def showImg(img, title): 
     plt.imshow(img,'gray'), plt.title(title),plt.axis('off')
+
+def transformPCA( vec, pca, n_comp):
+    return np.dot( vec- pca.mean_ , pca.components_[:n_comp].T)
     
-def run10Knn(X, y, Xtest, ytest ):
-    # fit using KNN-classifier,  check for best k (based on f1-score)
-    k_score= np.zeros([10,1])
-    k = range(1,11)
-
-    # knn with different K :
-    for curK in k:
-            clf = KNeighborsClassifier(curK)
-            with Timer('Fit k=' + str(curK) + ':'):
-                clf.fit(X,y)
-        
-            with Timer('Predict k=' + str(curK) + ':'):
-                pred = clf.predict(Xtest)
-
-            #k_f1[curK-1] = metrics.f1_score(ytest,pred, average='weighted')
-            k_score[curK-1] = metrics.accuracy_score(ytest,pred, average='weighted')
-    # show k vs f1 score  graph
-    plt.figure()
-    plt.plot(k, k_score)
-
-    
+def invTransormPCA(vec, pca, n_comp):
+    return np.dot(vec, pca.components_[:n_comp]) + pca.mean_
 
 ###  START OF SCRIPT :
 plt.close('all')
 
-# Load the dataset
+# intro: Load the dataset
 f = gzip.open('mnist.pkl.gz', 'rb')
 train_set, valid_set, test_set = cPickle.load(f)
 f.close()
@@ -67,8 +49,17 @@ f.close()
 x_train, y_train = train_set
 x_test, y_test = test_set
 
-# show first 12 images in db:
+## intro :
+# how many images there are for each digit?
 plt.figure(0)
+h = plt.hist(y_train,range(11))
+for i in range(10):
+    print str(i) + ": " +str(int(h[0][i]))
+
+plt.show()
+
+# show first 12 images in db:
+plt.figure(1)
 rows = 3    
 cols = 4
 
@@ -80,94 +71,104 @@ for i in range(rows):
         plt.subplot(rows,cols,idx+1)
         showImg(im, label)
         
-
 plt.show()
         
-        
-# how many images there are for each digit?
-plt.figure(1)
-h = plt.hist(y_train,range(11))
-for i in range(10):
-    print str(i) + ": " +str(int(h[0][i]))
+## A - run knn on data itself find best k: 
 
+# define a function that runs 10 knn (k=1..10) and plot k vs accuracy graph
+# (its a function for future use)
+def run10Knn(X, y, Xtest, ytest):
+    # fit using KNN-classifier,  check for best k (based on f1-score)
+    k_scores= np.zeros([10,1])
+    k = range(1,11)
+
+    # knn with different K :
+    for curK in k:
+            # set the n_jobs to -1 to maximize CPU usage, try algorithm='ball_tree' to make
+            # it go faster
+            clf = KNeighborsClassifier(n_neighbors=curK, n_jobs=-1, algorithm='ball_tree')
+            with Timer('Fit and calc accuracy score for k=' + str(curK) + ':'):
+                k_scores[curK-1]= clf.fit(X,y).score(Xtest,ytest)
+            
+    # show k vs  mean-accuracy score graph
+    plt.plot(k, k_scores), plt.xlabel('k'), plt.ylabel('mean-accuracy')
+    plt.title('num of nearest neighbors vs mean accuracy score')
+    plt.show()
+
+# run the function on the data itself:
+with Timer('run 10 times KNN on image data(witouth dim-reduction)'):
+    plt.figure(2)
+    run10Knn(x_train, y_train, x_test, y_test)
+
+
+## B do PCA on the train dataset and draw 6 first principle components
+pca = PCA()
+pca.fit(x_train)
+# plot mean image and 6 first priciple components:
+plt.figure(3)
+plt.subplot(2,4,1), showImg(pca.mean_.reshape([28,28]),'mean image')
+plt.subplot(2,4,2), showImg(pca.components_[0].reshape([28,28]),'1st component')
+plt.subplot(2,4,3), showImg(pca.components_[1].reshape([28,28]),'2nd component')
+plt.subplot(2,4,4), showImg(pca.components_[2].reshape([28,28]),'3rd component')
+plt.subplot(2,4,5), showImg(pca.components_[3].reshape([28,28]),'4th component')
+plt.subplot(2,4,6), showImg(pca.components_[4].reshape([28,28]),'5th component')
+plt.subplot(2,4,7), showImg(pca.components_[5].reshape([28,28]),'6th component')
+plt.subplot(2,4,8), showImg(pca.components_[6].reshape([28,28]),'7th component')
 plt.show()
 
+# C draw graph of total variance as function of p-components
+acc_var = np.cumsum(pca.explained_variance_ratio_) * 100.0
+plt.figure(4)
+plt.plot(acc_var), plt.xlabel('number of Eigenvectors included'), plt.ylabel('% variability captured')
+plt.title('% of variability of data Captured vs. Num of Eigenvectors')
+plt.show()
 
-## run on data itself : 
-#run10Knn(x_train, y_train, x_test, y_test)
-#
-## do PCA on the train dataset:
-#pca = PCA()
-#pca.fit(x_train)
-#
-## draw graph of total variance as function of p-components
-#acc_var = np.cumsum(pca.explained_variance_)
-#plt.figure(2)
-#plt.plot(acc_var)
-#
-## how many component needed for 95% or 80% variance:
-#acc_var_ratio = np.cumsum(pca.explained_variance_ratio_)
-#var95=  [ n for n,i in enumerate(acc_var_ratio) if i>0.95 ][0]
-#var80=  [ n for n,i in enumerate(acc_var_ratio) if i>0.90 ][0]
-#
-## plot mean image and 6 first priciple components:
-#plt.figure(3)
-#plt.subplot(2,4,1), showImg(pca.mean_.reshape([28,28]),'mean image')
-#plt.subplot(2,4,2), showImg(pca.components_[0].reshape([28,28]),'1st component')
-#plt.subplot(2,4,3), showImg(pca.components_[1].reshape([28,28]),'2nd component')
-#plt.subplot(2,4,4), showImg(pca.components_[2].reshape([28,28]),'3rd component')
-#plt.subplot(2,4,5), showImg(pca.components_[3].reshape([28,28]),'4th component')
-#plt.subplot(2,4,6), showImg(pca.components_[4].reshape([28,28]),'5th component')
-#plt.subplot(2,4,7), showImg(pca.components_[5].reshape([28,28]),'6th component')
-#plt.subplot(2,4,8), showImg(pca.components_[6].reshape([28,28]),'7th component')
-#plt.show()
-#
-## transform to 2 dimantions and scatter plot result:
-#pca = PCA(n_components=2)
-#x_train2dim = pca.fit_transform(x_train)
-#
-#digits = range(10)
-#colors = cm.rainbow(np.linspace(0, 1, len(digits)))
-#plt.figure(4)
-#for d, c in zip(digits, colors):
-#    plt.scatter(x_train2dim[y_train==d,0],x_train2dim[y_train==d,1], color=c)
-#    plt.hold('on')
-#
-#plt.hold('off')
-#plt.show()
-#
-#
-#comps = [2,10,20]
-#
-#for cc in comps:
-#    pca = PCA(n_components=cc)
-#    pca.fit(x_train)
-#    x_train_pca = pca.transform(x_train)
-#    x_test_pca = pca.transform(x_test)
-#    run10Knn(x_train_pca,y_train, x_test_pca, y_test)
-#    plt.title(str(cc) + ' compnents')
-#
-## g 
-pcaAll = PCA()
-pcaAll.fit(x_train)
-#
-##choose random sample
-#sampleIdx = np.uint(np.random.uniform(0,len(x_train)))
-#sample = x_train[sampleIdx:sampleIdx+1]
-#ks = [2,5,10,50,100,150,200]
-#
-#recon = np.zeros([len(ks), len(x_train[0])])
-#plt.figure(8)
-#plt.subplot(2,4,1), plt.imshow(sample.reshape([28,28]),'gray'), plt.axis('off'), plt.title('original')
-#for idx in range(len(ks)):
-#    num_dim = ks[idx]
-#    dec = np.dot( sample- pcaAll.mean_ , pcaAll.components_[:num_dim].T)
-#    rec = np.dot(dec, pcaAll.components_[:num_dim]) + pcaAll.mean_
-#    plt.subplot(2,4,idx+2), plt.imshow(rec.reshape([28,28]),'gray'), plt.axis('off'),
-#    plt.title('dims= ' + str(num_dim))
-#
-#plt.show()
-# h1:
+## D how many component needed for 95% or 80% variance:
+var95=  [ n for n,i in enumerate(acc_var) if i>95 ][0]
+var80=  [ n for n,i in enumerate(acc_var) if i>80 ][0]
+
+## E transform to 2 dimantions and scatter plot result:
+x_train2dim = transformPCA(x_train,pca,2)
+
+digits = range(10)
+colors = cm.rainbow(np.linspace(0, 1, len(digits)))
+plt.figure(5)
+for d, c in zip(digits, colors):
+    plt.scatter(x_train2dim[y_train==d,0],x_train2dim[y_train==d,1], color=c, label=str(d))
+    plt.hold('on')
+plt.legend()
+plt.hold('off')
+plt.show()
+
+# F - find best K  for Knn classifier running on PCA transformed train data with
+# 2,10 and 20 components
+comps = [2,10,20]
+for ccIdx in range(len(comps)):
+    cc = comps[ccIdx]
+    x_train_pca = transformPCA(x_train,pca,cc)
+    x_test_pca = transformPCA(x_test,pca,cc)
+    plt.figure(6+ccIdx)
+    run10Knn(x_train_pca,y_train, x_test_pca, y_test)
+    plt.title('# of NN vs accuracy : ' + str(cc) + ' compnents')
+    plt.show()
+
+## G - project random image to 2,5,10,50,100,150 dims and reconstruct 
+#choose random sample
+sampleIdx = np.uint(np.random.uniform(0,len(x_train)))
+sample = x_train[sampleIdx:sampleIdx+1]
+ks = [2,5,10,50,100,150,200]
+
+recon = np.zeros([len(ks), len(x_train[0])])
+plt.figure(9)
+plt.subplot(2,4,1), showImg(sample.reshape([28,28]),'original')
+for idx in range(len(ks)):
+    num_dim = ks[idx]
+    proj = transformPCA(sample,pca, num_dim)
+    rec = invTransormPCA(proj,pca, num_dim)
+    plt.subplot(2,4,idx+2), showImg(rec.reshape([28,28]), 'dims= ' + str(num_dim))
+
+plt.show()
+## H :
 class PCAPredictor:
     def __init__(self):
         pass 
@@ -188,7 +189,7 @@ class PCAPredictor:
         
 pcaPredictor = PCAPredictor()
 
-plt.figure(9)
+plt.figure(10)
 pcaPredictor.fit(x_train, y_train)
 
 for idx in range(10):
@@ -197,12 +198,10 @@ for idx in range(10):
         plt.subplot(11,6,6*idx + pcIdx + 1), showImg(model.components_[pcIdx].reshape([28,28]),str(pcIdx+1))
 
 for pcIdx in range(6):
-    plt.subplot(11,6,60 + pcIdx + 1),showImg(pcaAll.components_[pcIdx].reshape([28,28]),str(pcIdx+1))
+    plt.subplot(11,6,60 + pcIdx + 1),showImg(pca.components_[pcIdx].reshape([28,28]),str(pcIdx+1))
 
 plt.show()
 
-print metrics.classification_report(pcaPredictor.predict(test_set[0]), test_set[1])
-#h2 
-# transTest=
-#sampleIdx = np.uint(np.random.uniform(0,len(x_train)))
-    
+y_predict = pcaPredictor.predict(test_set[0])
+print metrics.classification_report(y_predict, test_set[1])
+print metrics.accuracy_score(y_predict, test_set[1])
--- maman12/part2.py
+++ maman12/part2.py
@@ -7,14 +7,15 @@ Created on Sun Dec 04 00:51:17 2016
 
 import time
 import cv2
+import os
 import numpy as np
-from sklearn.metrics import roc_curve, auc
 import glob
 from sklearn.cluster import KMeans
 from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import sklearn
+import matplotlib.cm as cm
 
 class Timer(object):
     def __init__(self, name=None):
@@ -37,6 +38,7 @@ def calcBOWDescriptor( features, codebook ):
 def calcSifts(samples, step_size):
     sift = cv2.xfeatures2d.SIFT_create()
     train_sifts = []
+    first = True
     for sample in samples :
         gray= sample[0]
     
@@ -44,58 +46,36 @@ def calcSifts(samples, step_size):
         kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, gray.shape[0], step_size) 
             for x in range(0, gray.shape[1], step_size)]
 
-        if (debug):
-            plt.imshow(cv2.drawKeypoints(gray,kp))
+        if (debug and first):
+            first = False
+            plt.figure(99)
+            plt.imshow(cv2.drawKeypoints(gray,kp,None))
             plt.show();
     
         kp2, dense_sift = sift.compute(gray,kp)
         train_sifts.append(dense_sift)
     
     # stack all train sifts in numpy matrix and return
-    return np.vstack(train_sifts), len(kp)
-
-    
-def train(train_samples, dsift_step_size, n_clusters,regTerm ):
-    with Timer('do dense-sift on all train data'):
-        # do dense-sift on all train data 
-        sifts, n_kp= calcSifts(train_samples, dsift_step_size)
-        
-    # perform kmeans on all samples
-    with Timer('Fit kmeans for vector-quantization'):
-        codebook = KMeans(n_clusters= n_clusters, verbose=1, tol=1, n_init=1)
-        codebook.fit(sifts)
-
-    # create centers histogram for each image :
-    with Timer('calc bow-descriptors'):
-        sample_desc = [] 
-        for sampleIdx in range(0, len(sifts),n_kp ):
-            features = sifts[sampleIdx:sampleIdx + n_kp]
-            sampleHist = calcBOWDescriptor(features, codebook )
-            sample_desc.append(sampleHist)
-                
-    X = np.vstack(sample_desc)
-    y =  np.asarray(train_samples)[:,1]
-    
-    # train svm :
-    with Timer('Fit SVM'):
-        clf = svm.LinearSVC(C=regTerm)
-        clf.fit(X, y.astype('int'))
-    return clf, codebook
+    return train_sifts, len(kp)
     
 def load_data( basedir , class_a, class_b, train_test_ratio):
     # load and split data 
     files = glob.glob(basedir + '/*.jpg')
     classes = []
     imgs = []
+    if os.name =='nt':
+        sep = '\\'
+    else :
+        sep = '/'
     for filename in files :
         # load image and convert to gray    
         img = plt.imread(filename)
         gray= cv2.cvtColor(img ,cv2.COLOR_BGR2GRAY)
         imgs.append(gray)
-        if filename.startswith(basedir +'/' +class_a):
+        if filename.startswith(basedir +sep +class_a):
+            classes.append(0)
+        elif filename.startswith(basedir +sep +class_b):
             classes.append(1)
-        elif filename.startswith(basedir +'/' +class_b):
-            classes.append(2)
 
     # attach samples to label
     dataset = zip(imgs, classes)
@@ -107,57 +87,83 @@ def load_data( basedir , class_a, class_b, train_test_ratio):
     n_train = int(len(dataset) * train_test_ratio)
     return dataset[:n_train], dataset[n_train:] 
 
-def detect(samples, codebook,  model, dsift_step_size ):
-    samplesDesc =[]
-    for sample in samples:
-        gray= sample[0][0]
-        # extract key points (dense)
-        features = calcSifts([gray],dsift_step_size)[0]
-        desc = calcBOWDescriptor(features, codebook)
-        samplesDesc.append(desc)
     
-    y= model.predict(samplesDesc)
-    
-    return y
-    
-    
-# params 
+###  START OF SCRIPT :
+
+# cleanup : 
+plt.close('all')
+
+## params 
 basedir = 'data'
 class_a = 'opencountry'
-class_b = 'tallbuilding'
-train_test_ratio = 0.8
+class_b = 'coast'
+train_test_ratio = 0.80
 dsift_step_size = 15
 debug = False
-n_clusters = 100
-regTerm = 1
+n_clusters = 150
+kmeans_tol = 1e-3 # 1
+regTerms = [0.0001, 0.001, 0.01, 0.1, 1, 5, 10 ]
 
+# load and split data:
 train_data, test_data = load_data(basedir,class_a, class_b, train_test_ratio)
 
+## TRAIN 
 # do dense-sift on all train data 
 with Timer('do dense-sift on all train data'):
     sifts, n_kp= calcSifts(train_data, dsift_step_size)
         
-# perform kmeans on all samples
+# perform kmeans on all samples (stack all sifts in one matrix)
 with Timer('Fit kmeans for vector-quantization'):
-    codebook = KMeans(n_clusters= n_clusters, verbose=1, tol=1, n_init=1)
-    codebook.fit(sifts)
+    codebook = KMeans(n_clusters= n_clusters, verbose=1, tol=kmeans_tol, n_init=1)
+    codebook.fit(np.vstack(sifts))
 
 # create centers histogram for each image :
-with Timer('calc bow-descriptors of'):
+with Timer('calc bow-descriptors of train'):
     sample_desc = [] 
-    for sampleIdx in range(0, len(sifts),n_kp ):
-        features = sifts[sampleIdx:sampleIdx + n_kp]
-        sampleHist = calcBOWDescriptor(features, codebook )
+    for sample in sifts:
+        sampleHist = calcBOWDescriptor(sample, codebook )
         sample_desc.append(sampleHist)
-                
+                    
     X_train = np.vstack(sample_desc)
     y_train =  np.asarray(train_data)[:,1].astype('int')
     
-# train svm :
-with Timer('Fit SVM with C='+str(regTerm)):
-    model = svm.LinearSVC(C=regTerm)
-    model.fit(X_train, y_train)
+# train svm in different reg-terms(C):
+models =[]
+for c in regTerms :
+    with Timer('Fit SVM with C='+str(c)):
+        model = svm.SVC(kernel = 'linear',C=c)
+        model.fit(X_train, y_train)
+        models.append(model)
 
+## TEST :
+with Timer('do dense-sift on all test data'):
+    sifts, n_kp= calcSifts(test_data, dsift_step_size)
 
-with Timer('Detect'):
-    detect(train_data, codebook, model, dsift_step_size)
+with Timer('calc BOW hist of all test data'):
+    sample_desc = [] 
+    for sample in sifts:
+        sampleHist = calcBOWDescriptor(sample, codebook )
+        sample_desc.append(sampleHist)
+                
+    X_test = np.vstack(sample_desc)
+    y_test =  np.asarray(test_data)[:,1].astype('int')
+
+auc = [];
+plt.figure(0)
+colors = cm.rainbow(np.linspace(0, 1, len(regTerms)))
+for modelIdx in range(len(models)):
+    c = colors[modelIdx]
+    model = models[modelIdx]    
+    y_score= model.decision_function(X_test)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_score)#, pos_label=1)    
+    auc.append(metrics.auc(fpr,tpr))
+    plt.hold('on')
+    plt.plot(fpr,tpr, color=c, label='C='+str(regTerms[modelIdx])+' auc='+str(auc[modelIdx])), plt.xlabel('FPR'), plt.ylabel('TPR')
+
+plt.hold('off')
+plt.title('ROC Curve')
+plt.legend()
+plt.figure(1)
+plt.plot(range(len(regTerms)), auc), plt.xlabel('C'), plt.ylabel('AUC'),plt.title('AUC vs. C val')
+plt.xticks(range(len(regTerms)),regTerms)
+
